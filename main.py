033a1f7b88c5a8b57ca574e58fe12893cdacd9fc
anas-rabhi <anas.rabhi.hakim@gmail.com>
2021-12-01 22:04:46 +0100

    add main idea

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+data = pd.read_csv('./data/books.csv')
 
-st.dataframe(dataframe.style.highlight_max(axis=1))
+st.sidebar.image(data.image_url[0])
+
+k = st.container()
+
+fr, sc = k.columns(2)
+
+
+#with st.expander('Notez les films :'):
+
+
+fr.image(data.image_url[0], caption = data.original_title[0])
+x = sc.slider(f'Rate {data.original_title[0]}', 1, 5)
+if x == 1: sc.markdown("""<div style="text-align: center"> ⭐ </div>""", unsafe_allow_html=True)
+if x == 2: sc.markdown("""<div style="text-align: center"> ⭐⭐ </div>""", unsafe_allow_html=True)
+if x == 3: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐ </div>""", unsafe_allow_html=True)
+if x == 4: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
+if x == 5: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
+
+#k2 = st.container()
+#k2.slider()
