9dbf0ffc098afe6fb8c985072f9a4c864ff746eb
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-12-04 00:53:58 +0100

    add some ideas

--- main.py
+++ main.py
@@ -23,24 +23,55 @@ books = generate_books_to_rate(data)
     #i[1].container
 #à revoir
 
-## add bouton to add random books or generate another sample
+## add bouton to add random books or generate another sample....
+
+## add bouton that takes into account the already watched among recommended movies and do another search
+
+#for i in books.iterrows():
+
+
+
+
 st.sidebar.image(data.image_url[0])
 
-k = st.container()
+#k = st.container()
 
-fr, sc = k.columns(2)
+#fr, sc = k.columns(2)
 
+d = {}
 
 #with st.expander('Notez les films :'):
+with st.container():
+    f, s = st.columns(2)
 
-
-fr.image(data.image_url[0], caption = data.original_title[0])
-x = sc.slider(f'Rate {data.original_title[0]}', 1, 5)
-if x == 1: sc.markdown("""<div style="text-align: center"> ⭐ </div>""", unsafe_allow_html=True)
-if x == 2: sc.markdown("""<div style="text-align: center"> ⭐⭐ </div>""", unsafe_allow_html=True)
-if x == 3: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐ </div>""", unsafe_allow_html=True)
-if x == 4: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
-if x == 5: sc.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
+    with f :
+        st.image(data.image_url[0], caption = data.original_title[0])
+    with s:
+        st.text(' ')
+        st.text(' ')
+        d[f'{data.original_title[0]}'] = st.slider(f'Rate {data.original_title[0]}', 1, 5)
+        if d[f'{data.original_title[0]}'] == 1: st.markdown("""<div style="text-align: center"> ⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[0]}'] == 2: st.markdown("""<div style="text-align: center"> ⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[0]}'] == 3: st.markdown("""<div style="text-align: center"> ⭐⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[0]}'] == 4: st.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[0]}'] == 5: st.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
 
 #k2 = st.container()
 #k2.slider()
+st.text(data.original_title[0])
+
+with st.container():
+    f, s = st.columns(2)
+
+    with f :
+        st.image(data.image_url[1], caption = 'data.original_title[1]')
+    with s:
+        st.text(' ')
+        st.text(' ')
+
+        d[f'{data.original_title[1]}'] = st.slider(f'Rate data.original_title[1]', 1, 5)
+        if d[f'{data.original_title[1]}'] == 1: st.markdown("""<div style="text-align: center"> ⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[1]}'] == 2: st.markdown("""<div style="text-align: center"> ⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[1]}'] == 3: st.markdown("""<div style="text-align: center"> ⭐⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[1]}'] == 4: st.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
+        if d[f'{data.original_title[1]}'] == 5: st.markdown("""<div style="text-align: center"> ⭐⭐⭐⭐⭐ </div>""", unsafe_allow_html=True)
