35a494dc9c82f841a0f936504e0eaf6dfddc8077
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-12-16 22:41:17 +0100

    update main

--- main.py
+++ main.py
@@ -4,50 +4,48 @@ import pandas as pd
 from book_app import BookDisplay
 import random
 
+if __name__ == '__main__':
+    data = pd.read_csv('./data/books.csv')
 
-data = pd.read_csv('./data/books.csv')
+    if 'number_of_books' not in st.session_state:
+        st.session_state['number_of_books'] = 5
 
+    if 'random_books' not in st.session_state:
+        st.session_state['random_books'] = random.randint(1,1000)
 
-
-if 'number_of_books' not in st.session_state:
-    st.session_state['number_of_books'] = 5
-
-if 'random_books' not in st.session_state:
-    st.session_state['random_books'] = random.randint(1,1000)
-
-if 'rated_books' not in st.session_state:
-    st.session_state['rated_books'] = {}
+    if 'rated_books' not in st.session_state:
+        st.session_state['rated_books'] = {}
 
 
-book_dis = BookDisplay()
+    book_dis = BookDisplay()
 
-search = st.text_input('Search a book by its title')
+    search = st.text_input('Search a book by its title')
 
 
 
-f, s = st.columns(2)
+    f, s = st.columns(2)
 
-#with f:
-#    if st.button('Load more books'):
-#        st.session_state['number_of_books'] += 5
-with s:
-    if st.button('Show another sample of books'):
-        st.session_state['random_books'] += 5
-        st.session_state['number_of_books'] = 5   # SHOW ANOTHER SAMPLE IS NOT WORKING THE FIRST TIME
+    #with f:
+    #    if st.button('Load more books'):
+    #        st.session_state['number_of_books'] += 5
+    with s:
+        if st.button('Show another sample of books'):
+            st.session_state['random_books'] += 5
+            st.session_state['number_of_books'] = 5   # SHOW ANOTHER SAMPLE IS NOT WORKING THE FIRST TIME
 
-# Generate books
-books_to_display, nb = BookDisplay.generate_books_to_rate(data, number_of_books=st.session_state['number_of_books'], rstate=st.session_state['random_books'], search=search)
+    # Generate books
+    books_to_display, nb = BookDisplay.generate_books_to_rate(data, number_of_books=st.session_state['number_of_books'], rstate=st.session_state['random_books'], search=search)
 
-st.write(f'Number of books available : {nb}')
-st.write(f'Number of books displayed : {st.session_state["number_of_books"]}')
-st.write(f'__________')
+    st.write(f'Number of books available : {nb}')
+    st.write(f'Number of books displayed : {st.session_state["number_of_books"]}')
+    st.write(f'__________')
 
-st.title(f'Books to rate')
+    st.title(f'Books to rate')
 
-# Display the books
-book_dis.display_books_to_rate(books_to_display, st.session_state['rated_books'])
+    # Display the books
+    book_dis.display_books_to_rate(books_to_display, st.session_state['rated_books'])
 
 
-# BookDisplay.recommended_books(st.session_state['rated_books'])
+    # BookDisplay.recommended_books(st.session_state['rated_books'])
 
-book_dis.display_books_to_recommend(rated_books=st.session_state['rated_books'], number_of_recommended=10)
+    book_dis.display_books_to_recommend(rated_books=st.session_state['rated_books'], number_of_recommended=10)
