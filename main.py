46e57a95443f2169097da096a72912fbbe7a9f84
anas-rabhi <anas.rabhi.hakim@gmail.com>
2021-12-22 20:33:23 +0100

    add comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 from book_app import BookDisplay
 import random
@@ -7,16 +6,20 @@ import random
 if __name__ == '__main__':
     data = pd.read_csv('./data/books.csv')
 
+    # Add number_of_books variable into session_state if does not exist (first run)
     if 'number_of_books' not in st.session_state:
         st.session_state['number_of_books'] = 5
 
+    # Add random_books variable into session_state if does not exist (first run)
     if 'random_books' not in st.session_state:
         st.session_state['random_books'] = random.randint(1, 1000)
 
+    # Add a dictionary of rated books to keep track of the rated books.
     if 'rated_books' not in st.session_state:
         st.session_state['rated_books'] = {}
 
-    book_dis = BookDisplay()
+    #
+    book_display = BookDisplay()
 
     search = st.text_input('Search a book by its title')
 
@@ -34,6 +37,7 @@ if __name__ == '__main__':
     books_to_display, nb = BookDisplay.generate_books_to_rate(data, number_of_books=st.session_state['number_of_books'],
                                                               rstate=st.session_state['random_books'], search=search)
 
+    # Display some useful information
     st.write(f'Number of books available : {nb}')
     st.write(f'Number of books displayed : {st.session_state["number_of_books"]}')
     st.write(f'__________')
@@ -41,8 +45,7 @@ if __name__ == '__main__':
     st.title(f'Books to rate')
 
     # Display the books
-    book_dis.display_books_to_rate(books_to_display, st.session_state['rated_books'])
+    book_display.display_books_to_rate(books_to_display, st.session_state['rated_books'])
 
     # BookDisplay.recommended_books(st.session_state['rated_books'])
-
-    book_dis.display_books_to_recommend(rated_books=st.session_state['rated_books'], number_of_recommended=10)
+    book_display.display_books_to_recommend(rated_books=st.session_state['rated_books'], number_of_recommended=10)
